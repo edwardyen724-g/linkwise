684107a1b2acc21c95f610170a693c497d85bc36
edwardyen724-g <65644998+edwardyen724-g@users.noreply.github.com>
2026-03-06 03:44:07 -0800

    feat: add src/lib/stripe.ts

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,7 +1,13 @@
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
-  apiVersion: '2022-11-15',
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
+
+if (!stripeSecretKey) {
+  throw new Error("Missing Stripe secret key in environment variables.");
+}
+
+const stripe = new Stripe(stripeSecretKey, {
+  apiVersion: '2022-11-15', // Adjust the API version according to your needs
 });
 
 export default stripe;
